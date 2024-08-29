cc19e4a15367ec11f004a4813fadb5f839e1c8ba
Rutvik <patelrutvik1702@gmail.com>
2024-08-29 16:41:50 -0700

    implement projects section

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,46 +1,101 @@
-import darkSaasLandingPage from "@/assets/images/dark-saas-landing-page.png";
+import floemaLandingPage from "@/assets/images/floema.jpg";
 import lightSaasLandingPage from "@/assets/images/light-saas-landing-page.png";
-import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
+import captionedCover from "@/assets/images/captioned-cover.png";
+import CheckCircleIcon from '@/assets/icons/check-circle.svg';
+import ArrowUpRight from '@/assets/icons/arrow-up-right.svg';
+import grainImage from '@/assets/images/grain.jpg';
+import Image from "next/image";
 
 const portfolioProjects = [
   {
-    company: "Acme Corp",
-    year: "2022",
-    title: "Dark Saas Landing Page",
-    results: [
-      { title: "Enhanced user experience by 40%" },
-      { title: "Improved site speed by 50%" },
-      { title: "Increased mobile traffic by 35%" },
-    ],
-    link: "https://youtu.be/4k7IdSLxh6w",
-    image: darkSaasLandingPage,
+    company: "One 7 [Rutvik.dev]",
+    year: "2024",
+    title: "Floema - An interactive jewellery store",
+    desc: "Made with 🧡",
+    skills: ['PUG', 'SCSS', 'WEBGL'],
+    link: "https://github.com/Rutvik17/Floema",
+    image: floemaLandingPage,
   },
   {
-    company: "Innovative Co",
-    year: "2021",
+    company: "One 7 [Rutvik.dev]",
+    year: "2024",
     title: "Light Saas Landing Page",
-    results: [
-      { title: "Boosted sales by 20%" },
-      { title: "Expanded customer reach by 35%" },
-      { title: "Increased brand awareness by 15%" },
-    ],
-    link: "https://youtu.be/7hi5zwO75yc",
+    desc: "Made with 🩵",
+    skills: ['Next.js', 'Tailwind CSS'],
+    link: "https://rutvik17.github.io/saas-landing-page/",
     image: lightSaasLandingPage,
   },
   {
-    company: "Quantum Dynamics",
-    year: "2023",
-    title: "AI Startup Landing Page",
-    results: [
-      { title: "Enhanced user experience by 40%" },
-      { title: "Improved site speed by 50%" },
-      { title: "Increased mobile traffic by 35%" },
-    ],
-    link: "https://youtu.be/Z7I5uSRHMHg",
-    image: aiStartupLandingPage,
+    company: "One 7 [Rutvik.dev]",
+    year: "2024",
+    title: "Captioned - Generate media captions",
+    desc: "Made with 💕",
+    skills: ['React Native', 'Gemini', 'Reanimated'],
+    link: "https://github.com/Rutvik17/Captioned",
+    image: captionedCover,
   },
 ];
 
 export const ProjectsSection = () => {
-  return <div>Projects Section</div>;
+  return (
+    <section className="pb-16 lg:py-24">
+      <div className="container">
+        <div className="flex justify-center">
+          <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-blue-300 to-emerald-400 text-transparent bg-clip-text">My Personal Projects</p>
+        </div>
+        <h2 className="font-serif text-3xl text-center mt-6 md:text-5xl">Featured Projects</h2>
+        <p className="text-center text-white/60 mt-4 md:text-lg max-w-md mx-auto lg:text-xl">See how I transformed concepts into engaging digital experiences.</p>
+        <div className="flex flex-col mt-10 md:mt-20 gap-20">
+          {portfolioProjects.map((project) => {
+            return (
+              <div key={project.title} className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 px-8 pt-8 after:pointer-events-none md:pt-12 md:px-10 lg:pt-16 lg:px-20">
+                <div className="absolute inset-0 -z-10 opacity-5" style={{
+                  backgroundImage: `url(${grainImage.src})`,
+                }}></div>
+                <div className="lg:grid lg:grid-cols-2 lg:gap-16">
+                  <div className="lg:pb-16">
+                    <div className="uppercase bg-gradient-to-r from-blue-300 to-emerald-400 text-transparent bg-clip-text inline-flex gap-2 font-bold tracking-widest text-sm">
+                      <span>{project.company}</span>
+                      <span>&bull;</span>
+                      <span className="italic font-serif">{project.year}</span>
+                    </div>
+                    <h3 className="font-serif text-2xl mt-2 md:mt-5 md:text-4xl">
+                      {project.title}
+                    </h3>
+                    <hr className="border-t-2 border-white/5 mt-4 md:mt-5" />
+                    <span className="font-serif text-sm mt-2 italic">
+                      {project.desc}
+                    </span>
+                    <ul className="flex gap-4 mt-4 md:mt-5 flex-wrap">
+                      {project.skills.map((skill) => {
+                        return (
+                          <li key={skill} className="flex gap-2 bg-gradient-to-r from-blue-300 to-blue-400 p-2 rounded-xl items-center">
+                            <CheckCircleIcon className='size-5 text-blue-400 md:size-6' />
+                            <span className="text-sm md:text-base text-white/70">
+                              {skill}
+                            </span>
+                          </li>
+                        )
+                      })}
+                    </ul>
+                    <a href={project.link}>
+                      <button className="bg-white text-gray-950 h-12 w-full md:w-auto px-6 rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8">
+                        <span>
+                          Visit Live Site
+                        </span>
+                        <ArrowUpRight className='size-4' />
+                      </button>
+                    </a>
+                  </div>
+                  <div className="relative">
+                    <Image src={project.image} alt={project.title} className="mt-8 -mb-4 md:-mb-0 lg:mt-0 lg:absolute lg:h-full lg:w-auto lg:max-w-none rounded-xl" />
+                  </div>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    </section>
+  )
 };

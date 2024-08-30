4a9c91a891c7876fe7b934dc031660e09f1913a6
Rutvik <patelrutvik1702@gmail.com>
2024-08-29 17:06:59 -0700

    implement tape section

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -1,3 +1,36 @@
+import StarIcon from '@/assets/icons/star.svg';
+
+const words = [
+  'Performant',
+  'Accessible',
+  'Secure',
+  'Interactive',
+  'Scalable',
+  'User Friendly',
+  'Responsive',
+  'Maintainable',
+  'Search Optimized',
+  'Usable',
+  'Reliable'
+];
+
 export const TapeSection = () => {
-  return <div>Tape Section</div>;
+  return (
+    <section className='py-16 lg:24 overflow-x-clip'>
+      <div className='bg-gradient-to-r from-blue-300 to-emerald-400 -rotate-3 -mx-1'>
+        <div className='flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent))'>
+          <div className='flex flex-none gap-4 py-3'>
+            {words.map(word => {
+              return (
+                <div key={word} className='inline-flex gap-4 items-center'>
+                  <span className='text-gray-900 font-extrabold uppercase text-sm'>{word}</span>
+                  <StarIcon className='size-6 text-gray-900 -rotate-12' />
+                </div>
+              )
+            })}
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 };

6fbcf88b0e07b6556fa34fce3043d6f8d5998b95
Rutvik <patelrutvik1702@gmail.com>
2024-09-03 23:27:36 -0700

    animate stars in the hero section

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,11 +1,17 @@
 import ArrowUpRightIcon from '@/assets/icons/arrow-up-right.svg';
 import grainImage from '@/assets/images/grain.jpg';
+import { SectionHeader } from '@/components/SectionHeader';
 
 export const ContactSection = () => {
   return (
     <section className='py-16 pt-12 lg:py-24 lg:pt-20' id='contact'>
       <div className="container">
-        <div className='bg-gradient-to-r from-blue-300 to-sky-400 text-gray-900 py-8 px-10 rounded-3xl text-center md:text-left relative overflow-hidden z-10'>
+        <SectionHeader
+          title="Contact Me"
+          eyebrow="Like my work!?"
+          description="Connect with me today."
+        />
+        <div className='bg-gradient-to-r from-blue-300 to-sky-400 text-gray-900 py-8 px-10 rounded-3xl text-center md:text-left relative overflow-hidden z-10 mt-10 md:mt-20'>
           <div className="absolute inset-0 opacity-5 -z-10" style={{
             backgroundImage: `url(${grainImage.src})`
           }} />
@@ -16,9 +22,9 @@ export const ContactSection = () => {
             </div>
             <div>
               <a href='mailto:patelrutvik1702@gmail.com' className='text-white bg-gray-900 inline-flex items-center px-6 h-12 rounded-xl gap-2 mt-8 w-max border border-gray-900'>
-                <a href='mailto:patelrutvik1702@gmail.com' className='font-semibold'>
+                <span className='font-semibold'>
                   Contact Me
-                </a>
+                </span>
                 <ArrowUpRightIcon className='size-4' />
               </a>
             </div>
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -7,25 +7,27 @@ import SparkleIcon from '@/assets/icons/sparkle.svg';
 import { Memoji } from '@/components/Memoji';
 
 const stars = [
-  { size: 800, rotation: -72, className: 'size-28 text-blue-300' },
-  { size: 550, rotation: 20, className: 'size-12 text-blue-300' },
-  { size: 590, rotation: 98, className: 'size-8 text-blue-300' }
+  { size: 590, rotation: 20, className: 'size-12 text-blue-300', shouldOrbit: true, orbitDuration: '50s', shouldSpin: true, spinDuration: '12s' },
+  { size: 690, rotation: 98, className: 'size-8 text-blue-300', shouldOrbit: true, orbitDuration: '50s', shouldSpin: true, spinDuration: '12s' },
+  { size: 720, rotation: -140, className: 'size-16 text-blue-300', shouldOrbit: true, orbitDuration: '50s', shouldSpin: true, spinDuration: '12s' },
+  { size: 800, rotation: -72, className: 'size-28 text-blue-300', shouldOrbit: true, orbitDuration: '50s', shouldSpin: true, spinDuration: '12s' },
 ]
 
 const sparkles = [
-  { size: 430, rotation: -14, className: 'size-8 text-blue-300/20' },
-  { size: 430, rotation: 79, className: 'size-5 text-blue-300/20' },
-  { size: 530, rotation: 178, className: 'size-10 text-blue-300/20' },
-  { size: 710, rotation: 144, className: 'size-14 text-blue-300/20' }
+  { size: 420, rotation: -14, className: 'size-8 text-blue-300/20', shouldOrbit: true, orbitDuration: '54s', shouldSpin: true, spinDuration: '12s' },
+  { size: 550, rotation: 79, className: 'size-5 text-blue-300/20', shouldOrbit: true, orbitDuration: '54s', shouldSpin: true, spinDuration: '12s' },
+  { size: 590, rotation: 178, className: 'size-10 text-blue-300/20', shouldOrbit: true, orbitDuration: '54s', shouldSpin: true, spinDuration: '12s' },
+  { size: 710, rotation: 144, className: 'size-14 text-blue-300/20', shouldOrbit: true, orbitDuration: '54s', shouldSpin: true, spinDuration: '12s' },
+  { size: 840, rotation: 60, className: 'size-14 text-blue-300/20', shouldOrbit: true, orbitDuration: '54s', shouldSpin: true, spinDuration: '12s' }
 ]
 
 const dots = [
-  { size: 720, rotation: 85, className: 'size-3 rounded-full bg-blue-300/20' },
-  { size: 520, rotation: -41, className: 'size-2 rounded-full bg-blue-300/20' },
-  { size: 650, rotation: -5, className: 'size-2 rounded-full bg-blue-300/20' }
+  { size: 520, rotation: -41, className: 'size-2 rounded-full bg-blue-300/20', shouldOrbit: true, orbitDuration: '58s', shouldSpin: false, spinDuration: '12s' },
+  { size: 650, rotation: -165, className: 'size-2 rounded-full bg-blue-300/20', shouldOrbit: true, orbitDuration: '58s', shouldSpin: false, spinDuration: '12s' },
+  { size: 720, rotation: 85, className: 'size-3 rounded-full bg-blue-300/20', shouldOrbit: true, orbitDuration: '58s', shouldSpin: false, spinDuration: '12s' },
+  { size: 790, rotation: -5, className: 'size-2 rounded-full bg-blue-300/20', shouldOrbit: true, orbitDuration: '58s', shouldSpin: false, spinDuration: '12s' },
+  { size: 820, rotation: -120, className: 'size-2 rounded-full bg-blue-300/20', shouldOrbit: true, orbitDuration: '58s', shouldSpin: false, spinDuration: '12s' }
 ]
-
-
 
 export const HeroSection = () => {
   return (
@@ -43,7 +45,15 @@ export const HeroSection = () => {
 
         {stars.map((star, index) => {
           return (
-            <HeroOrbit size={star.size} rotation={star.rotation} key={index}>
+            <HeroOrbit
+              size={star.size}
+              rotation={star.rotation}
+              key={index}
+              shouldOrbit={star.shouldOrbit}
+              orbitDuration={star.orbitDuration}
+              shouldSpin={star.shouldSpin}
+              spinDuration={star.spinDuration}
+            >
               <StarIcon className={star.className} />
             </HeroOrbit>
           )
@@ -51,7 +61,15 @@ export const HeroSection = () => {
 
         {sparkles.map((sparkle, index) => {
           return (
-            <HeroOrbit size={sparkle.size} rotation={sparkle.rotation} key={index}>
+            <HeroOrbit
+              size={sparkle.size}
+              rotation={sparkle.rotation}
+              key={index}
+              shouldOrbit={sparkle.shouldOrbit}
+              orbitDuration={sparkle.orbitDuration}
+              shouldSpin={sparkle.shouldSpin}
+              spinDuration={sparkle.spinDuration}
+            >
               <SparkleIcon className={sparkle.className} />
             </HeroOrbit>
           )
@@ -59,8 +77,18 @@ export const HeroSection = () => {
 
         {dots.map((dot, index) => {
           return (
-            <HeroOrbit size={dot.size} rotation={dot.rotation} key={index}>
-              <div className={dot.className} />
+            <HeroOrbit
+              size={dot.size}
+              rotation={dot.rotation}
+              key={index}
+              shouldOrbit={dot.shouldOrbit}
+              orbitDuration={dot.orbitDuration}
+              shouldSpin={dot.shouldSpin}
+              spinDuration={dot.spinDuration}
+            >
+              <div className={`relative ${dot.className}`}>
+                <div className={`absolute inset-0 ${dot.className} animate-ping-large`}></div>
+              </div>
             </HeroOrbit>
           )
         })}
@@ -70,7 +98,9 @@ export const HeroSection = () => {
         <div className='flex flex-col items-center'>
           <Memoji />
           <div className='bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg'>
-            <div className='bg-green-500 size-2.5 rounded-full'></div>
+            <div className='bg-green-500 size-2.5 rounded-full relative'>
+              <div className="bg-green-500 size-2.5 inset-0 rounded-full absolute animate-ping-large"></div>
+            </div>
             <div className='text-sm font-medium'>RUTVIK PATEL</div>
           </div>
         </div>
